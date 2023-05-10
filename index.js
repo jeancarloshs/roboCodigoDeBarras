76016381e7f1a3edc4c5df78cb4c76fe000b8e8a
Jean <jeancarlos.na@hotmail.com>
2023-05-09 21:43:38 -0300

    criado json de materias e artigos de apelaria

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 
 let chrome = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
 let userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36';
-let urlComosBlueSoft = 'https://cosmos.bluesoft.com.br/gpcs/calcados-63000000/produtos?page=1'; 
+let targetUrl = 'https://cosmos.bluesoft.com.br/gpcs/artigos-de-papelaria-equipamentos-para-escritorio-suprimentos-de-ocasiao-62000000/produtos';
+let urlComosBlueSoft = `${targetUrl}?page=1`; 
 // let nomeCategoria = document.querySelectorAll('.page-header > a');
 let arrayJson = []
 
